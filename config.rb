4ba82ee0322fe4d04de81d9b724c07d20bd17c62
Preeti Yuankrathok <preetisatit@gmail.com>
2020-07-20 23:07:25 +0700

    Add livereload

--- config.rb
+++ config.rb
@@ -20,6 +20,9 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :fonts_dir, 'fonts'
 
+# Activate auto reload
+activate :livereload, js_host: '0.0.0.0'
+
 # Activate the syntax highlighter
 activate :syntax
 ready do
